0bfbd0c68c4051ce19dfe8e536b0ec102428cd0a
giospes <giovannijacopo.spes@gmail.com>
2023-05-04 16:42:58 +0200

    printing all the info

--- index.php
+++ index.php
@@ -8,6 +8,66 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
+    <?php
+
+        $hotels = [
+
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+
+        ];
+
+    ?>  
     
+
+    <main>
+        <?php
+            foreach($hotels as $hotel){
+        ?>
+            <ul>
+                <li><h2><?php echo $hotel['name']; ?></h2></li>
+                <li><?php echo $hotel['description']; ?></li>
+                <li><?php echo $hotel['parking']; ?></li>
+                <li><?php echo $hotel['vote']; ?></li>
+                <li><?php echo $hotel['distance_to_center']; ?></li>
+
+            </ul>
+        <?php
+            }
+        ?>
+    </main>
 </body>
 </html>
